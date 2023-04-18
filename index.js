05cfe126337f4ce1485ee2c51bafc8d7f5b8f361
DrakeDrac <50480704+DrakeDrac@users.noreply.github.com>
2023-04-18 15:17:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { App } from "@tinyhttp/app";
+import { json } from "milliparsec";
 
 const app = new App();
 
 app
+  .use(json())
   .post("/", (req, res) => {
     console.log(req.body);
 
